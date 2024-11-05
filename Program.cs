8d362bb35c6f0c230b7d8d31006ff72e7db6ab43
Iwaku-Real <128941248+Iwaku-Real@users.noreply.github.com>
2024-11-04 21:13:38 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using MessagePack;
+using MessagePack.Resolvers;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text.Json;
+
+namespace HubApiTest
+{
+    internal class Program
+    {
+        static async Task<object> HubFunction(HubConnection hub, string func, params string[] args)
+        {
+            if (args.Length == 0)
+            {
+                return await hub.InvokeAsync<object>(func);
+            }
+            else
+            {
+                return await hub.InvokeCoreAsync<object>(func, args);
+            }
+        }
+        static void Main()
+        {
+            HubConnection hub = new HubConnectionBuilder().AddMessagePackProtocol(delegate (MessagePackHubProtocolOptions options)
+            {
+                options.SerializerOptions =
+                MessagePackSerializerOptions.Standard.WithCompression(MessagePackCompression.Lz4Block)
+                .WithResolver(ContractlessStandardResolver.Instance);
+            }
+            ).WithUrl("https://stepfordcountyrailway.co.uk/Push/Meta")
+            .Build();
+
+            hub.StartAsync().Wait();
+
+            while (true)
+            {
+                string cmd = Console.ReadLine();
+                string[] split = cmd.Split(' ');
+
+                object result;
+                try
+                {
+                    result = HubFunction(hub, split[0], split.Skip(1).ToArray()).GetAwaiter().GetResult();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error occured while processing command!");
+                    Console.WriteLine(ex.ToString());
+                    continue;
+                }
+                //Console.WriteLine(result);
+                Console.WriteLine(JsonSerializer.Serialize(result, new JsonSerializerOptions { WriteIndented = true }));
+                //Thread.Sleep(1000);
+            }
+        }
+    }
+}
